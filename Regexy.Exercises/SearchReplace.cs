f6494ca881b6b32a5fd3d7934447e1879cc772e9
Oscar Olsson <happybits@gmail.com>
2020-10-09 15:05:04 +0200

    Uppgift 3

--- Regexy.Exercises/SearchReplace.cs
+++ Regexy.Exercises/SearchReplace.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Regexy.Exercises
 {
@@ -32,7 +27,9 @@ namespace Regexy.Exercises
         {
             /*
             
-            Remove Name="...."
+            Remove:
+            
+                Name="...."
 
             E.g
 
@@ -51,7 +48,58 @@ namespace Regexy.Exercises
             };
 
             var searchFor = @"\(\s*Name\s*=\s*""[^""]*""[,\s]*";
+
             var replaceWith = "(";
+
+            var result = Engine.SearchReplace(ex, searchFor, replaceWith);
+            Assert.IsTrue(result.Correct);
+
+            // Alternativ lösning
+
+            var searchForAlternative = @"\(\s*Name\s*=\s*"".*?""[,\s]*";  // non greedy
+            var result2 = Engine.SearchReplace(ex, searchForAlternative, replaceWith);
+            Assert.IsTrue(result2.Correct);
+
+        }
+
+        [TestMethod]
+        public void remove_required()
+        {
+            /*
+            
+                Remove 
+                
+                    [Required(...)]
+
+                E.g replace this
+
+                        [Display(Name = "Tilldela aktiviteten till denna roll.", Order = 2)]
+                        [Required(AllowEmptyStrings = false)]
+                        [Enum(typeof(Enums.AssignedActivityRole))]
+
+                with this
+
+                        [Display(Name = "Tilldela aktiviteten till denna roll.", Order = 2)]
+                        [Enum(typeof(Enums.AssignedActivityRole))]
+
+
+                Answer
+
+                    Search      \n\s*\[Required\(.*\)\]\s*
+                    Replace
+
+         
+             
+             */
+            var ex = new SearchReplaceExercise
+            {
+                Id = 3,
+                Name = "Remove required",
+                Description = "",
+            };
+
+            var searchFor = @"\[Required\(.*\)\]\s*\n\s*"; 
+            var replaceWith = "";
 
             var result = Engine.SearchReplace(ex, searchFor, replaceWith);
             Assert.IsTrue(result.Correct);
